f1f157d386b8a412a85bd78fda84b6a873d21c53
trevor <trevor@kali.trevor>
2022-05-16 11:29:42 +0200

    Task 0.7

---
+++ task_0.7.py
@@ -0,0 +1,22 @@
+#   Temperature Conversion Formula -> (0°C × 9 / 5) + 32 = 32°F
+
+def celsius_to_fahrenheit(temp):
+    temperature_in_celsius = float(temp)
+    convert_to_fahrenheit = (temperature_in_celsius * (9/5)) + 32
+
+    #   print(convert_to_fahrenheit)
+    return f"{convert_to_fahrenheit :.3f} Degrees in Fahrenheit"
+
+
+def fahrenheit_to_celsius(temp):
+    temperature_in_fahrenheit = float(temp)
+    convert_to_celsius = (temperature_in_fahrenheit - 32) / (9/5)
+    return f"{convert_to_celsius :.3f} Degrees in Celsius"
+
+
+fahrenheit = celsius_to_fahrenheit(42)
+#   print(fahrenheit)
+celsius = fahrenheit_to_celsius(107.600)
+#   print(celsius)
+
+
