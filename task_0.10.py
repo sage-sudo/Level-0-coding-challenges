5f46fcef79cbdd35042e34c39ba6c87aa8dab0ea
trevor <trevor@kali.trevor>
2022-05-16 13:04:23 +0200

    Task 0.10

---
+++ task_0.10.py
@@ -0,0 +1,11 @@
+def grab_common_chars(string1, string2):
+    common_characters = list()
+
+    for string in string2:
+        if string in string1 and string not in common_characters:
+            common_characters.append(string)
+
+    print("Common letters:", ", ".join(common_characters))
+
+
+grab_common_chars("house", "computers")
