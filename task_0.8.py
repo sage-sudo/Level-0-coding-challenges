54d2acf4c2f89c0584bf753a457b305c4c19249f
sage-sudo <77978611+sage-sudo@users.noreply.github.com>
2022-05-17 19:22:23 +0200

    Update task_0.8.py
    Fixing Task 0.8: Check for spelling errors

--- task_0.8.py
+++ task_0.8.py
@@ -1,8 +1,8 @@
-def time_conversion(num):
+def time_convertion(num):
     number = int(num)
 
-    handlesPlurasForHours = "hour"
-    handlesPlurasForMinuets = "minuet"
+    handles_pluras_for_hours = "hour"
+    handles_pluras_for_minuets = "minuet"
 
     count_hours = 0
 
@@ -12,17 +12,17 @@ def time_conversion(num):
 
     take_minuets = number
 
-    if count_hours > 1:
-        handlesPlurasForHours = "hours"
+    if count_hours > 1 or count_hours == 0:
+        handles_pluras_for_hours = "hours" 
 
-    if take_minuets > 1:
-        handlesPlurasForMinuets = "minuets"
+    if take_minuets > 1 or take_minuets == 0:
+        handles_pluras_for_minuets = "minuets"
 
-    time = f"{count_hours} {handlesPlurasForHours}, {take_minuets} {handlesPlurasForMinuets}."
+    time = f"{count_hours} {handles_pluras_for_hours}, {take_minuets} {handles_pluras_for_minuets}."
     #   print(time)
     return time
 
 
-timed = time_conversion(133)
+timed = time_convertion(133)
 
 print(timed)
