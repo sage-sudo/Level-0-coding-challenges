9717a0a2cb82e2b70e1f842ac1c0a9a3d472fe0a
sage-sudo <77978611+sage-sudo@users.noreply.github.com>
2022-05-18 08:04:51 +0200

    Update task_0.5.py
    Fixed Task 0.5: Use the formula that takes in 3 sides. The formula you've used only works for 1 type of triangle. Print(checkArea) so the area is displayed.

--- task_0.5.py
+++ task_0.5.py
@@ -1,11 +1,25 @@
-def area_of_triangle(opposite, base, hypotenuse):
-    area = 1/2 * (float(base) * float(hypotenuse))
-    return area
+import math
 
+# Formula for parameter : s = 1/2 (a+b+c)
+# Formula for Area : A = square_root(s (s - a) (s - b) (s - c))
+
+
+def triangle_area_by_sides(opposite, base, hypotenuse):
+    opposite = float(opposite)
+    base = float(base)
+    hypotenuse = float(hypotenuse)
+
+    parameter = 1/2 * (opposite + base + hypotenuse)
+
+    are_of_triangle = math.sqrt(parameter * ((parameter - opposite) * (parameter - base) * (parameter - hypotenuse)))
+    
+    return are_of_triangle
+
+    
 
 try:
-    checkArea = area_of_triangle(2.5, 3, 5)
-    #   print(f"Area is: {checkArea}")
+    checkArea = triangle_area_by_sides(3, 4, 5)
+    print(f"Area is: {checkArea}")
 
 except (ValueError, NameError):
     print("Maybe try with a different value (-_0)")
